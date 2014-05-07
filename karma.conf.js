bf85d79ccb817833d27502dc6e28ca97e6c1ef64
Mathieu ROBIN <mthrobin@gmail.com>
2014-05-07 21:04:06 +0200

    more tests

--- karma.conf.js
+++ karma.conf.js
@@ -34,6 +34,6 @@ module.exports = function (config) {
         logLevel         : config.LOG_INFO,
         browsers         : ['Chrome', 'Firefox'],
         autoWatch        : true,
-        singleRun        : true
+        singleRun        : false
     });
 };
